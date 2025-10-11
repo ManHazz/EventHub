a05977c562d15227a0c45ee467219164cb8a48c0
raziq <raziqsufian2002@gmail.com>
2025-10-11 15:27:48 +0800

    connect mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+connectDB().then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
 });
