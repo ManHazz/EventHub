e6b979ef764dcb614d759c12ecee1ff29cecb1d0
raziq <raziqsufian2002@gmail.com>
2025-10-11 22:08:35 +0800

    created event schema

--- backend/src/models/event.js
+++ backend/src/models/event.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
-const eventSchena = new mongoose.Schema({
+const eventSchema = new mongoose.Schema({
+    bookingId: {type:String},
+    name: {type:String},
+    desccription: {type:String},
+    date: {type:Date},
+    time: {type:String},
+    location: {type:String},
+    price: {type:Number},
+    totalSeats: {type:Number},
+    status: {type:String, enum: ['confirm', 'pending'], default: 'pending'},
 
 });
 
-const Event = mongoose.model("Event", eventSchena);
+const Event = mongoose.model("Event", eventSchema);
 
 export default Event;
