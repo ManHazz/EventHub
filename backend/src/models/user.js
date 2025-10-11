46aca2ebd89fd9f9b90b57656330337744bfe523
raziq <raziqsufian2002@gmail.com>
2025-10-11 22:20:23 +0800

    created user schema

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
+    role: {type:String, enum: ['user', 'admin'], default: 'user'},
+    name: {type:String, required:true},
+    email: {type:String, required:true, unique:true},
+    password: {type:String, required:true},
+    totalBookings: {type:Number, default:0},
+    bookedEvents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Event'}],
+    status: {type:String, enum: ['active', 'inactive'], default: 'active'},
 })
 
 const User = mongoose.model("User", userSchema);
