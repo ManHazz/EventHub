563f692f8049f66d1fe3f2d0151ea5b55a683fdb
raziq <raziqsufian2002@gmail.com>
2025-10-12 14:09:39 +0800

    add all logic on users controller

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -64,26 +64,75 @@ export const loginUser = async (req, res) => {
     }
 }
 
-export const getUserProfile = (req, res) => {
+export const getUserProfile = async (req, res) => {
     try {
+        const user = await User.findById(req.user.id)
+
+        if(!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        res.status(200).json(user);
         
     } catch (error) {
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const getAllUserProfile = async (req, res) => {
+    
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: "Forbidden: Admins only" });
+    }
+
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const updateUserProfile = async(req, res) => {
+    try {
+        const user = await User.findById (req.params.id);
+        if(!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        const {name, email, password, confirmPassword,status} = req.body;
+
+        if(password && password !== confirmPassword) {
+            return res.status(400).json({message: "Passwords do not match"});
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        if(password) {
+            user.password = password;
+        }
+        user.role = role || user.role;
+        user.status = status || user.status;
+
+        await user.save();
+
+        res.status(200).json({message: "User profile updated successfully"});
         
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"});
     }
 }
 
-export const updateUserProfile = (req, res) => {
+export const deleteUser = async (req, res) => {
     try {
-        
-    } catch (error) {
-        
-    }
-}
+        const user = await User.findById(req.params.id);
+        if(!user) {
+            return res.status(404).json({message: "User not found"});
+        }
 
-export const deleteUser = (req, res) => {
-    try {
-        
+        await user.remove();
+        res.status(200).json({message: "User deleted successfully"});
     } catch (error) {
-        
+        res.status(500).json({message: "Internal server error"});
     }
 }
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile, updateUserProfile, deleteUser } from '../controllers/userControllers.js';
+import { registerUser, loginUser, getUserProfile, updateUserProfile, deleteUser, getAllUserProfile } from '../controllers/userControllers.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser); //register user
 router.post('/login',loginUser); //login user
-router.get('/profile', getUserProfile); //get user profile
-router.put('/profile', updateUserProfile); //update user profile
-router.delete('/profile', deleteUser); //delete user
+router.get('/profile', protect, getUserProfile); //get user profile
+router.get('/allProfile', protect, getAllUserProfile); //get all user profile (for admin)
+router.put('/profile/:id', protect, updateUserProfile); //update user profile
+router.delete('/profile/:id', protect, deleteUser); //delete user
 
 export default router;
