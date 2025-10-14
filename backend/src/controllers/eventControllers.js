f4c6965a1ec6a549ce4ebcc98d3eb49ba074cf1e
raziq <raziqsufian2002@gmail.com>
2025-10-14 23:35:11 +0800

    add booking controller, update other controller

--- backend/src/controllers/eventControllers.js
+++ backend/src/controllers/eventControllers.js
@@ -36,6 +36,9 @@ export const getUserEvents = async (req, res) => {
 export const getAllEvents = async (req, res) => {
     try {
         const events = await Event.find({});
+        if(events.length === 0) {
+            return res.status(404).json({message: "No events available"});
+        }
         res.status(200).json(events);
     } catch (error) {
         res.status(500).json({message: "Internal server error"});
@@ -44,10 +47,35 @@ export const getAllEvents = async (req, res) => {
 
 export const getAllBookings = async (req, res) => {
     try {
-        const allUser = await User.find({}).select('name').populate('bookedEvents', 'bookingId name date price status');
-        res.status(200).json(allUser);
+        const usersWithBookings = await User.find({ 
+            bookedEvents: { $exists: true, $ne: [] } 
+        })
+        .select('name') 
+        .populate({
+            path: 'bookedEvents',
+            select: 'bookingId name date price status' 
+        });
+
+        if (!usersWithBookings || usersWithBookings.length === 0) {
+            return res.status(404).json({ message: "No book event yet" });
+        }
+
+        const allBookings = usersWithBookings.flatMap(user => 
+            user.bookedEvents.map(event => ({
+                bookingId: event.bookingId,
+                userName: user.name,
+                eventName: event.name,
+                date: event.date,
+                price: event.price,
+                status: event.status
+            }))
+        );
+
+        res.status(200).json(allBookings);
+
     } catch (error) {
-        res.status(500).json({message: "Internal server error"});
+        console.error(error); 
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
@@ -88,3 +116,38 @@ export const updateEvent = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const bookEvent = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const event = await Event.findById(id);
+        if(!event) {
+            return res.status(404).json({message: "Event not found"});
+        }
+
+        const user = await User.findById(req.user.id);
+        if(!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        if(user.bookedEvents.includes(event._id)) {
+            return res.status(400).json({message: "Event already booked"});
+        }
+
+        user.bookedEvents.push(event._id);
+        user.totalBookings += 1;
+        event.totalSeats -= 1;
+        if(event.totalSeats < 0) {
+            return res.status(400).json({message: "No seats available"});
+        }
+        await user.save();
+
+        event.status = 'confirm';
+        await event.save();
+
+        res.status(200).json({message: "Event booked successfully", event, user});
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"});
+    }
+}
--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import { createEvent, getUserEvents, getAllEvents, deleteEvent, updateEvent, getAllBookings } from '../controllers/eventControllers.js';
+import { createEvent, getUserEvents, getAllEvents, deleteEvent, updateEvent, getAllBookings, bookEvent } from '../controllers/eventControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
 
-router.post('/create', createEvent); //create event
-router.get('/userEvents', getUserEvents); //get all events for a user(user dashboard)
-router.get('/allEvents', getAllEvents); //get all events(admin dashboard)
-router.get('/allBookings', getAllBookings); //get all bookings (admin dashboard)
-router.delete('/delete/:id', deleteEvent); //delete event
-router.put('/update/:id', updateEvent); //update event
+router.post('/create', protect, createEvent); //create event
+router.get('/userEvents', protect, getUserEvents); //get all events for a user(user dashboard)
+router.get('/allEvents', protect, getAllEvents); //get all events(admin dashboard, display on homepage)
+router.get('/allBookings', protect, getAllBookings); //get all users bookings (admin dashboard)
+router.post('/book/:id', protect, bookEvent); //book event (id of event to be booked)
+router.delete('/delete/:id', protect, deleteEvent); //delete event
+router.put('/update/:id', protect, updateEvent); //update event
 
 export default router;
 
