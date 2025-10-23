308ec611a71c6c458720560910143d766672d7d4
ManHazz <a.ahazimin@gmail.com>
2025-10-23 10:32:57 +0800

    admin page

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -5,8 +5,10 @@ export default function Admin() {
   return (
     <>
       <Navbar />
-      <div className="bg-gradient-to-br from-blue-100 to-blue-300 flex flex-col items-center w-full px-20 sm:px-6 lg:px-8 py-25">
-        <div></div>
+      <div className="bg-gradient-to-br from-blue-100 to-blue-300 w-full px-20 sm:px-6 lg:px-8 py-25">
+        <h1 className="flex align-start text-4xl font-bold text-gray-900 mb-6">
+          Admin Panel
+        </h1>
       </div>
     </>
   );
